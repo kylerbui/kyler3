86a49fec8c5c2d62fcadb0226b07b8ae94968a68
kylerbui <kylerbui04@gmail.com>
2016-08-26 17:52:06 -0700

    Create s.py

---
+++ s.py
@@ -0,0 +1,17 @@
+# import special libraries already built in python
+import random, urllib2
+
+# list of options to select from
+the_url = 'https://raw.githubusercontent.com/kylerbui/kyler1/master/activities.lst'
+list_raw_text = urllib2.urlopen(the_url).read()
+
+print "DEBUG:  " + str(list_raw_text.split())
+
+possible_activities = ['a','b','c']
+
+# choice of what we are going to do
+the_activity = random.choice(possible_activities)
+
+# display the results to the end user
+print "Possible activities are: " + str(possible_activities)
+print "What we are going to do:" + the_activity
